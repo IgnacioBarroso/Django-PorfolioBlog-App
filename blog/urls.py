3ef2225156bff0bfc95cf812ca1362ec1b434ca3
Ignacio Barroso <ignacio_barroso@outlook.com>
2021-11-18 18:52:15 -0300

    Finished porfolio demo

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import render_posts, render_post
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', render_posts, name='posts'),
     path('<int:post_id>', render_post, name='post'),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -7,5 +7,5 @@ def render_posts(request):
     return render(request, 'posts.html', {'posts': posts})
 
 def render_post(request, post_id):
-    post = Post.objects.get(id=post_id)
-    return render(request, 'posts.html', {'post': post})
+    post = get_object_or_404(Post, pk=post_id) 
+    return render(request, 'post.html', {'post': post})
